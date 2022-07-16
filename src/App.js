44edd69f2abdb96976d572c7400c816d10e6c55d
Caroline Van <cmvan@stanford.edu>
2022-07-16 14:38:30 -0700

    adhere to eslint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
-
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import * as Pages from './pages'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as Pages from './pages';
 
 function App() {
   return (
