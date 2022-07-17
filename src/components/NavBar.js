06c0558fda5cd9aea26742e6a1067a4c3be77b9f
Caroline Van <cmvan@stanford.edu>
2022-07-17 15:47:53 -0700

    made basic navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,51 +1,59 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavScrollExample() {
+function NavScroll() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">Stanford</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
+        <Navbar.Collapse className="justify-content-end" id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <NavDropdown title="Team">
+              <NavDropdown.Item as={Link} to="/team">Team</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/attributions">Attributions</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/collaborations">Collaborations</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <NavDropdown title="Project">
+              <NavDropdown.Item as={Link} to="/communication">Communication</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/contribution">Contribution</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/description">Description</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/engineering">Engineering</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/experiments">Experiments</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/implementation">Implementation</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/notebook">Notebook</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/partnership">Partnership</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/proof-of-concept">Proof of Concept</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/results">Results</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Parts">
+              <NavDropdown.Item as={Link} to="/part-collection">Part Collection</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/parts">Parts</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/improve">Improve</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link as={Link} to="/safety">Safety</Nav.Link>
+            <Nav.Link as={Link} to="/human-practices">Human Practices</Nav.Link>
+            <NavDropdown title="Awards">
+              <NavDropdown.Item as={Link} to="/education">Education</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/entrepreneurship">Entrepreneurship</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/inclusivity">Inclusivity</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/measurement">Measurement</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/model">Model</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/software">Software</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/sustainable">Sustainable</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default NavScrollExample;
+export default NavScroll;
